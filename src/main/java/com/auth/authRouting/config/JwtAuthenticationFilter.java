d87c7569c06a012485071c78eebf979a58cf8c88
Calin <chelcea.calin@yahoo.com>
2023-08-06 09:14:18 +0300

    Only request if authenticated

--- src/main/java/com/auth/authRouting/config/JwtAuthenticationFilter.java
+++ src/main/java/com/auth/authRouting/config/JwtAuthenticationFilter.java
@@ -26,7 +26,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserService userService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
--- src/main/java/com/auth/authRouting/controller/TestController.java
+++ src/main/java/com/auth/authRouting/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/auth/employees")
+@RequestMapping("/api/employees")
 public class TestController {
 
     @GetMapping("/list")
